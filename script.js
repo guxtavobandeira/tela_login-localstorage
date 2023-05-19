017548c80538ca9720559388ea7c656e623e585a
guxtavobandeira <111713549+guxtavobandeira@users.noreply.github.com>
2023-05-19 08:47:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var Csn = document.getElementById("Senha")
 var Eem = document.getElementById("EEmail")
 var Esn = document.getElementById("ESenha")
 window.addEventListener("load", function () {
-
+    
     //dashboard.textContent = exibir()
 
     var str = ""
@@ -10,7 +10,7 @@ window.addEventListener("load", function () {
         let oi = localStorage.key(i)
         let S = localStorage.getItem(oi)
         ///str += `${[i + 1]}. Cadastro de: ${oi}<br>ㅤㅤEmail: ${S}  <br>`
-        /* let use = localStorage.getItem(eemail)
+        let use = localStorage.getItem(eemail)
         if (use) {
             console('ok')
             dashboard.innerHTML += `
@@ -18,10 +18,10 @@ window.addEventListener("load", function () {
             <div id="userEmail">Email: ${S}</div>
             `
         }
-        const nameDash = document.getElementById('userName')
+         const nameDash = document.getElementById('userName')
         const emailDash = document.getElementById('userEmail')
         nameDash.innerHTML = `${[i + 1]}. Cadastro de: ${oi}`
-        emailDash.innerHTML = `Email: ${S}` */
+        emailDash.innerHTML = `Email: ${S}`
     }
 })
 
@@ -32,45 +32,26 @@ loginForm.addEventListener('submit', function (event) {
     const usuario = document.getElementById('eemail').value;
     const senha = document.getElementById('ssenha').value;
     const cadastro = JSON.parse(localStorage.getItem(usuario));
-    function real() {
-        
-    }
-    var Ds = 0
-    var Dc = 0
-    var De = 0
-    for (let i = 0, len = localStorage.length; i < len; i++) {
-        let oi = localStorage.key(i)
-        let S = localStorage.getItem(oi)
 
-        if (!usuario || !senha) {
-            //  return showToast('error', 'Campos incompletos');
-            Ds = Ds + 1
-        
-
-            // return showToast('error', 'Usuário ou senha incorreta');
-        } else if (oi == senha && S == usuario) {
-            
-            // const dashboard = document.getElementById('dadosContainer')
-            // var str = ""
-            Dc = Dc + 1
+    if (!usuario || !senha) {
+        //  return showToast('error', 'Campos incompletos');
+    } else if ((cadastro === null) || (CryptoJS.SHA256(senha).toString() !== cadastro.senha)) {
+        // return showToast('error', 'Usuário ou senha incorreta');
+    } else {
+        const dashboard = document.getElementById('dadosContainer')
+        var str = ""
+        for (let i = 0, len = localStorage.length; i < len; i++) {
+            let oi = localStorage.key(i)
+            let S = localStorage.getItem(oi)
             ///str += `${[i + 1]}. Cadastro de: ${oi}<br>ㅤㅤEmail: ${S}  <br>`
-            /* dashboard.innerHTML += `
+            dashboard.innerHTML += `
                 <div id="userName">${[i + 1]}. Cadastro de: ${oi}</div>
                 <div id="userEmail">Email: ${S}</div>
-                ` */
-        }else{
-            De = De + 1 
+                `
         }
         /* const nameDash = document.getElementById('userName')
         const emailDash = document.getElementById('userEmail')
         nameDash.innerHTML = `${[i + 1]}. Cadastro de: ${oi}`
         emailDash.innerHTML = `Email: ${S}` */
-    }
-    if(Ds > 0){
-        alert("Sem dados")
-    }else if(Dc > 0){
-        alert("Seja bem vindo")
-    }else{
-        alert("Dados errados ou inclompletos")
     }
 });
